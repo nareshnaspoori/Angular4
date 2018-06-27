edab53d70f42976e29e71caa6bd43b7d013f11b2
Naras Naspoori <nnaspoor@meredith.com>
2018-06-27 18:22:11 +0530

    anglular 4 with asp.net mvc 5

---
+++ ng2Mvc5Demo-master/ng2Mvc5Demo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ng2Mvc5Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TempData["index"] = "we are in Index Page";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            if (TempData["index"]!=null)
+            {
+                ViewBag.Message = TempData["index"];
+                TempData.Keep("index");
+            }
+         
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            if (TempData["index"] != null)
+            {
+                ViewBag.Message = TempData["index"];
+            }
+
+            return View();
+        }
+    }
+}
